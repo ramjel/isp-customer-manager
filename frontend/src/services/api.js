d1ccb0ee9c20282fa22df17ec20a51c075eaf2aa
Ramzelle Casuga <ramzelle2@gmail.com>
2026-07-03 10:56:18 +0800

    force API baseURL fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,29 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: "https://isp-backend-8xzm.onrender.com/api",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401 && !error.config.url?.includes('/auth/login')) {
-      localStorage.removeItem('token');
-      if (window.location.pathname !== '/login') {
-        window.location.href = '/login';
-      }
-    }
-    return Promise.reject(error);
-  }
-);
+console.log("API IS USING:", api.defaults.baseURL);
 
 export default api;
